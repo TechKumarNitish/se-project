d90ba7b083250e8176712887cbb5be44de766a6c
TechKumarNitish <rskban15a2015@gmail.com>
2025-03-14 21:15:21 +0530

    initial commit

---
+++ client/src/layouts/AdminLayout/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Divider, Typography, Link } from '@material-ui/core';
+import useStyles from './styles';
+
+export default function Footer() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Divider />
+      <Typography className={classes.copyright} variant="body1">
+        &copy; SE Project. 2025
+      </Typography>
+      <Typography variant="caption">
+      Designed with precision and excellence |{' '}
+        <Link href="http://georgesimos.com/" target="_blank" rel="noopener">
+          SE Project
+        </Link>
+      </Typography>
+    </div>
+  );
+}
---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const simple = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, 'mySecret');
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token,
+    });
+    if (!user) throw new Error();
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send({ error: 'Please authenticate.' });
+  }
+};
+
+const enhance = async (req, res, next) => {
+  try {
+    console.log("header",req.header('Authorization'));
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, 'mySecret');
+    console.log("decoded",decoded);
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token,
+    });
+    console.log("user",user);
+    if (!user || user.role !== 'superadmin') throw new Error();
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log("not authorized")
+    res.status(401).send({ error: 'Please authenticate.' });
+  }
+};
+
+module.exports = { simple, enhance };

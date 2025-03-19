c6242a88b42212106a03c74b98fef705508d6d28
TechKumarNitish <rskban15a2015@gmail.com>
2025-03-19 09:54:02 +0530

    shifted db to mongo atlas, updated client npm build cmd

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/se-project", {
+mongoose.connect(process.env.MONGODB_URI
+  , {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
   useCreateIndex: true,
-});
+}); 
